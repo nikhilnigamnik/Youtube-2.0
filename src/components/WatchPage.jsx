bf17761d3e49e6616e42d29a58b0159d796c408d
nikhilnigamnik <nikhilnigamnik@gmail.com>
2023-06-06 21:10:57 +0530

    pages

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useParams, useSearchParams } from "react-router-dom";
+import { YOUTUBE_VIDEO_ID_API } from "../utils/constant";
 
 const WatchPage = () => {
   const params = useParams();
   const [searchParams] = useSearchParams();
+  const [videos, setVideos] = useState([]);
 
   const dispatch = useDispatch();
 
@@ -13,15 +15,26 @@ const WatchPage = () => {
     dispatch(closeMenu());
   }, [dispatch]);
 
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  const getVideos = async () => {
+    const data = await fetch(YOUTUBE_VIDEO_ID_API);
+    const json = await data.json();
+    console.log(json.items);
+    setVideos(json.items);
+  };
+
   return (
     <div className="justify-center items-center flex">
       <iframe
         className="w-[80vw] h-[80vh]"
         src={"https://www.youtube.com/embed/" + searchParams.get("v")}
         title="YouTube video player"
-        frameborder="0"
+        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen
+        allowFullScreen
       ></iframe>
     </div>
   );
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,3 +2,6 @@ const GOOGLE_API_KEY = "AIzaSyDMy7NBrUccnQDxLp_zsLC5h7b5GCzBSiM";
 export const YOUTUBE_VIDEO_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   GOOGLE_API_KEY;
+export const YOUTUBE_VIDEO_ID_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc&key=" +
+  GOOGLE_API_KEY;
